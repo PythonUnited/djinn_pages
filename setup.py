797515d48205186d53f21499ce9d08c942df6582
Martin Bouma <bouma@egoactive.com>
2015-09-30 22:14:41 +0200

    preparing release 1.0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requires = [
     ]
 
 setup(name='djinn_pages',
-      version="1.0.0",
+      version="1.0.2",
       description='Djinn Intranet Pages',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
