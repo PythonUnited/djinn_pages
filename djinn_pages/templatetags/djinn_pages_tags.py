a59d4b1ade82f31d4edc7d96b0d354ba2d481d3a
Martin Bouma <martin@studioreactor.com>
2022-04-14 11:21:44 +0200

    added templatetag for menu in new design

--- djinn_pages/templatetags/djinn_pages_tags.py
+++ djinn_pages/templatetags/djinn_pages_tags.py
@@ -5,6 +5,16 @@ register = Library()
 
 @register.inclusion_tag('djinn_pages/snippets/menuitem.html', takes_context=True)
 def custommenu(context, parent=None):
+    """
+    DEPRECATED
+    """
+    menuitems = MenuItem.objects.filter(parent = parent)
+
+    context.update({'menuitems': menuitems})
+    return context
+
+@register.inclusion_tag('gronet_v3/includes/menuitem.html', takes_context=True)
+def custommenu_v3(context, parent=None):
 
     menuitems = MenuItem.objects.filter(parent = parent)
 
